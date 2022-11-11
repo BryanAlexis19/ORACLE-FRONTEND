0e5677cc64458ca4c2e250f65c3f2ac3f5c321ad
BryanAlexis19 <librale19@gmail.com>
2022-11-10 22:00:29 -0500

    Validity input lesson

--- 7 JS WEB/1842-html-validaciones-proyecto_inicial/assets/js/script.js
+++ 7 JS WEB/1842-html-validaciones-proyecto_inicial/assets/js/script.js
@@ -1,14 +1,61 @@
 export const valida = (input) => {
-    const tipoDeInput = input.dataset.tipo;
-    if(validadores[tipoDeInput]) {
-        validadores[tipoDeInput](input);
+  const tipoDeInput = input.dataset.tipo;
+  if (validadores[tipoDeInput]) {
+    validadores[tipoDeInput](input);
+  }
+
+  if(input.validity.valid) {
+    input.parentElement.classList.remove("input-container--invalid")
+    input.parentElement.querySelector(".input-message-error").innerHTML = "";
+  }else{
+    input.parentElement.classList.add("input-container--invalid");
+    input.parentElement.querySelector(".input-message-error").innerHTML = mostrarMensajeDeError(tipoDeInput, input);
+  }
+
+};
+
+const tipoDeErrores = [
+    "valueMissing",
+    "typeMismatch",
+    "patternMismatch",
+    "customError"
+]
+
+const mensajeDeError = {
+    nombre: {
+        valueMissing: "El campo de nombre puede estar vacio"
+    },
+    email: {
+        valueMissing: "El campo de email no puede estar vacio",
+        typeMismatch: "El correo no es valido"
+    },
+    password: {
+        valueMissing: "El campo de contraseña no puede estar vacio",
+        patterMissmatch: "Mínimo 8 caracteres y máximo 12. Al menos una letra y un número"
+    },
+    nacimiento: {
+        valueMissing: "El campo de nacimiento no puede estar vacio",
+        customError: "Debe tener al menos 18 anios de edad"
     }
+
 }
 
 const validadores = {
-    nacimiento: (input) => verificarMayoriaEdad(input),
-}
+  nacimiento: (input) => verificarMayoriaEdad(input),
+};
 
+const mostrarMensajeDeError = (tipoDeInput, input) => {
+    let mensaje = "";
+        tipoDeErrores.forEach( error => {
+            if(input.validity[error]){
+                console.log(error)
+                console.log(input.validity[error])
+                console.log(mensajeDeError[tipoDeInput][error])
+                mensaje = mensajeDeError[tipoDeInput][error]
+            }
+        })
+    return mensaje;
+}
 
 // //Obtener el input de la fecha
 // inputNacimiento = window.document.querySelector("#birth");
@@ -19,12 +66,12 @@ const validadores = {
 
 //Identificar evento de cambio de fecha
 const verificarMayoriaEdad = (nacDateInput) => {
-    const fechaUsuario = new Date(nacDateInput.value);
-    let msj = "";
-    if(!isMayorEdad(fechaUsuario)){
-        msj = "Debe tener al menos 18 anios";
-    }
-    nacDateInput.setCustomValidity(msj);
+  const fechaUsuario = new Date(nacDateInput.value);
+  let msj = "";
+  if (!isMayorEdad(fechaUsuario)) {
+    msj = "Debe tener al menos 18 anios";
+  }
+  nacDateInput.setCustomValidity(msj);
 };
 
 const isMayorEdad = (fecha) => {
